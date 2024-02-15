248412e071959ab63a4686941ee1e4712a8d8163
joyp25 <160171215+joyp25@users.noreply.github.com>
2024-02-15 14:30:44 -0500

    Add files via upload

---
+++ lab_assignment_5.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	char letter;
+	struct node* next;
+} node;
+
+// Returns number of nodes in the linkedList.
+int length(node* head)
+{
+	int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+// parses the string in the linkedList
+//  if the linked list is head -> |a|->|b|->|c|
+//  then toCString function wil return "abc"
+char* toCString(node* head)
+{
+	int len = length(head);
+    char* str = (char*)malloc((len + 1) * sizeof(char)); // +1 for null terminator
+    if (str == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = 0;
+    while (head != NULL) {
+        str[i++] = head->letter;
+        head = head->next;
+    }
+    str[i] = '\0'; // null terminator
+    return str;
+}
+
+// inserts character to the linkedlist
+// f the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as foolows:
+// head -> |a|->|b|->|c|->|x|
+void insertChar(node** pHead, char c)
+{
+	node* newNode = (node*)malloc(sizeof(node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->letter = c;
+    newNode->next = NULL;
+
+    if (*pHead == NULL) {
+        *pHead = newNode;
+    } else {
+        node* current = *pHead;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+// deletes all nodes in the linkedList.
+void deleteList(node** pHead)
+{
+	while (*pHead != NULL) {
+        node* temp = *pHead;
+        *pHead = (*pHead)->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int i, strLen, numInputs;
+    node* head = NULL;
+    char* str;
+    char c;
+    FILE* inFile = fopen("input.txt","r");
+
+    if (inFile == NULL) {
+        fprintf(stderr, "Error opening input file.\n");
+        return EXIT_FAILURE;
+    }
+
+    fscanf(inFile, "%d\n", &numInputs);
+    
+    while (numInputs-- > 0) {
+        fscanf(inFile, "%d\n", &strLen);
+        for (i = 0; i < strLen; i++) {
+            fscanf(inFile, "%c", &c);
+            insertChar(&head, c);
+        }
+
+        str = toCString(head);
+        printf("String is : %s\n", str);
+
+        free(str);
+        deleteList(&head);
+
+        if (head != NULL) {
+            printf("deleteList is not correct!\n");
+            break;
+        }
+    }
+
+    fclose(inFile);
+    return EXIT_SUCCESS;
+}
